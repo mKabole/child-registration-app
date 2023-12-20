b9df161ba3431c2ffe0059480c29e7fb36467e12
mKabole <makungukabole34@gmail.com>
2023-12-20 12:40:20 +0200

    initial commit

--- child-registration/src/components/Navbar.jsx
+++ child-registration/src/components/Navbar.jsx
@@ -6,17 +6,18 @@ import {
   Button,
   IconButton,
 } from "@material-tailwind/react";
- 
+import { Link } from 'react-router-dom'
+
 export function NavbarDefault() {
   const [openNav, setOpenNav] = React.useState(false);
- 
+
   React.useEffect(() => {
     window.addEventListener(
       "resize",
       () => window.innerWidth >= 960 && setOpenNav(false),
     );
   }, []);
- 
+
   const navList = (
     <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       <Typography
@@ -37,35 +38,25 @@ export function NavbarDefault() {
             fill="#90A4AE"
           />
         </svg>
- 
+
         <a href="/list" className="flex items-center">
           Children List
         </a>
       </Typography>
     </ul>
   );
- 
+
   return (
     <Navbar className="mx-auto max-w-screen-xl px-4 py-2 lg:px-8 lg:py-4">
       <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
         <Typography
           as="a"
           href="/"
-          className="mr-4 cursor-pointer py-1.5 font-medium"
+          className="mr-4 cursor-pointer py-1.5 font-bold"
         >
           Child Registration
         </Typography>
         <div className="hidden lg:block">{navList}</div>
-        <div className="flex items-center">
-          <Button
-            variant="gradient"
-            size="xs"
-            color="green"
-            className="hidden lg:inline-block"
-          >
-            <Typography className="" href="/">Register Child</Typography>
-          </Button>
-        </div>
         <IconButton
           variant="text"
           className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
@@ -107,11 +98,6 @@ export function NavbarDefault() {
       <MobileNav open={openNav}>
         <div className="container mx-auto">
           {navList}
-          <div className="flex items-center gap-x-1">
-            <Button color="green" fullWidth variant="gradient" size="sm" className="">
-              <span>Register</span>
-            </Button>
-          </div>
         </div>
       </MobileNav>
     </Navbar>
